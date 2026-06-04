a465b447696ae47088a2eeed24013bd42de719a9
sankari282006-debug <sankari282006@gmail.com>
2026-06-05 00:16:49 +0530

    day 1 java practice

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+public static void main(String[] args) {
+    int variable = 10;
+    System.out.println("hello world");
+    System.out.print("iM gonna achevie my goals");
+    System.out.println(variable);
+
+}}
