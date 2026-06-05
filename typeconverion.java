194bc01c4f573d9252da6bd88989bc058b893561
sankari282006-debug <sankari282006@gmail.com>
2026-06-05 12:16:54 +0530

    type conversion and casting

---
+++ typeconverion.java
@@ -0,0 +1,12 @@
+public class typeconverion {
+    public static void main (String args[]){
+    int b = 19;
+    byte c = 30;
+    float d= 6.6f;
+
+    b=(int)d; 
+    //we could able to assign the float to a int cause int has bigger bit value comoared to float but not int to float as int has 4 byte 
+    System.out.println(b);
+}
+    
+}
