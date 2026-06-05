e78f083fd27ab063cda211dfa5008d6f7e2bb665
sankari282006-debug <sankari282006@gmail.com>
2026-06-05 09:15:16 +0530

    --day 2 pratice

---
+++ variable.java
@@ -0,0 +1,11 @@
+public class variable {
+    public static void main (String args[]){
+        int num =6;
+        float num2 =6.6f;
+        double num3 = 3.6;
+        double result = num + num2 + num3;
+        System.out.println(result);
+    }
+
+    
+}
