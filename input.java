4083bbd3a678345d0d9a5600e4ad2d799049f28d
sankari282006-debug <sankari282006@gmail.com>
2026-06-06 20:59:11 +0530

    input from user-1

---
+++ input.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+class input{
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("enter your number ");
+        int num = sc.nextInt(); 
+        System.out.println(""+num+" ");
+        System.out.println(""+num  +num+"  ");
+        System.out.println(""+num +num +num+"   ");
+        
+
+    }
+}
