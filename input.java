135570e191f91abf32dc2fd0ec57ac02e9ad376f
sankari282006-debug <sankari282006@gmail.com>
2026-06-07 20:45:41 +0530

    addtion using scanner statement

--- input.java
+++ input.java
@@ -1,15 +1,11 @@
-import java.util.Scanner;
-
-
+import java.util.*;
 class input{
-    public static void main(String[] args){
-        Scanner sc = new Scanner (System.in);
-        System.out.println("enter your number ");
-        int num = sc.nextInt(); 
-        System.out.println(""+num+" ");
-        System.out.println(""+num  +num+"  ");
-        System.out.println(""+num +num +num+"   ");
-        
-
-    }
-}
+    public static void main(String[] args) {
+        Scanner rj = new Scanner(System.in);
+       System.out.println("enter the value for a");
+       int a = rj.nextInt ();
+       System.out.println("enter the value for b");
+       int b = rj.nextInt();
+       int result = a+b;
+       System.out.println("sum of a and b is"+result);
+    }}
