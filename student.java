cda0e15a034169a773bd5374cde59a02fbebc17a
sankari282006-debug <sankari282006@gmail.com>
2026-06-17 08:27:08 +0530

    classes and object

---
+++ student.java
@@ -0,0 +1,19 @@
+class student{
+
+   int regno;
+   String name;
+   float cgpa;
+
+  
+
+ public static void main(String[] args) {
+
+    student student = new student();
+    student.regno=8;
+    student.name ="sankari";
+    student.cgpa=8.2f;
+    System.out.println(student.regno);
+    System.out.println(student.name);
+    System.out.println(student.cgpa);
+   }
+}
