1320bd421ea43752be5665b53059e09baf4f5ac8
sankari282006-debug <sankari282006@gmail.com>
2026-06-17 10:14:56 +0530

    method practice qn 2

--- method.java
+++ method.java
@@ -1,13 +1,12 @@
 public class method {
 
-    static int sum(int a , int b){
-       int add= a+  b;
-       return(add);
+    static void greetings(){
+        
+       
+       System.out.println("helloo");
 
     }
     public static void main(String[] args) {
-        int oper =sum(99,98);
-        System.out.println("the sum of the two numbers is"+ sum(87, 990));
-        System.out.println(oper);
+        greetings();
     }
 }
