118755adb61913c004fc632e67e2a1a799783c8f
sankari282006-debug <sankari282006@gmail.com>
2026-06-17 10:33:10 +0530

    method practice qn 3

--- method.java
+++ method.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class method {
 
-    static void greetings(){
-        
-       
-       System.out.println("helloo");
-
-    }
+    static boolean check(int a){
+        if(a>10 && a<100){
+            return true;
+        }
+        else
+        return false;
+         }
     public static void main(String[] args) {
-        greetings();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("pls enter the value of a");
+        int a = scanner.nextInt();
+      boolean value= check(a);
+        System.out.println(value);
     }
 }
